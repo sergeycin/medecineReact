b296fd86f0d5c0dc99cc009a02761a57520225d1
sergeycin <67867645+sergeycin@users.noreply.github.com>
2023-01-19 13:32:51 +0300

    hub

--- src/templates/main/main.tsx
+++ src/templates/main/main.tsx
@@ -5,29 +5,42 @@ import analize from '../../assets/img/analize.svg'
 import { makeRequest } from '../../hooks/fetch.hooks';
 import { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 function Main() {
 
   useEffect (()=>{ 
 
-    fetch('http://dev.rulis.club:4028/api/login2.json', {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        body: JSON.stringify({'folderno':'0000333189','password':'25672','api-key': 'ba4deeb3-e2a1-4f8e-8b44-4ffb6455ed48'})
-    })  .then(data =>{
+    axios.post('http://dev.rulis.club:4028/api/login2.json', {
+      ffolderno: '0000333189',
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    // fetch('http://dev.rulis.club:4028/api/login2.json', {
+    //     method: 'POST',
+    //     mode: 'cors',
+    //     cache: 'no-cache',
+    //     headers: {
+    //       "Content-Type": "application/json"
+    //   },
+    //     body: JSON.stringify({'folderno':'0000333189','password':'25672','api-key': 'ba4deeb3-e2a1-4f8e-8b44-4ffb6455ed48'})
+    // })  .then(data =>{
       
-      console.log(data)
+    //   console.log(data)
     
-      })
-      .then( data => {
-          console.log(data)
+    //   })
+    //   .then( data => {
+    //       console.log(data)
         
         
-    })
-    .catch(function(error){
-        console.log(error)
-    })
+    // })
+    // .catch(function(error){
+    //     console.log(error)
+    // })
 
         },[])
   // const data = makeRequest('http://127.0.0.1:4028/api/lis/login2.json','POST',{'folderno':'22333','password':'23232323232','api-key': 'ba4deeb3-e2a1-4f8e-8b44-4ffb6455ed48'})
